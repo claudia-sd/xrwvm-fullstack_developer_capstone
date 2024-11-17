8b42ec741f74b689d2d867b003d4647363dd127c
claudia-sd <claudiasentinelli@gmail.com>
2024-11-16 20:24:48 -0500

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -55,7 +55,7 @@ def initiate():
          "year": 2023, "car_make": car_make_instances[4]},
         # Add more CarModel instances as needed
     ]
-    
+
     for data in car_model_data:
         CarModel.objects.create(name=data['name'],
                                 car_make=data['car_make'],
