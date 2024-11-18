2f18aa1c027053c6c94659e5671262d30a0da881
claudia-sd <claudiasentinelli@gmail.com>
2024-11-17 19:50:14 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -22,10 +22,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -35,7 +35,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -107,20 +107,19 @@ try {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id + 1;
 
-  const review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
+  const review = new Reviews();
+	review.id = new_id;
+	review.name = data['name'];
+	review.dealership = data['dealership'];
+	review.review = data['review'];
+	review.purchase = data['purchase'];
+	review.purchase_date = data['purchase_date'];
+	review.car_make = data['car_make'];
+	review.car_model = data['car_model'];
+	review.car_year = data['car_year'];
 
   try {
     const savedReview = await review.save();
